fabcccfd0e9967cf563ede830f311b910d4e2d41
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-02-04 14:35:51 +0000

    Fix PRI*PTR and SCN*PTR for CHERI purecap
    Not sure why each architecture needs to have a separate file with the same
    bugs but refactoring it would be too much work.

--- sys/riscv/include/_inttypes.h
+++ sys/riscv/include/_inttypes.h
@@ -36,6 +36,12 @@
  * Macros for format specifiers.
  */
 
+#ifndef __CHERI_PURE_CAPABILITY__
+#define	__PRIptr	"l"
+#else
+#define	__PRIptr	"P"
+#endif
+
 /* fprintf(3) macros for signed integers. */
 
 #define	PRId8		"d"	/* int8_t */
@@ -51,7 +57,7 @@
 #define	PRIdFAST32	"d"	/* int_fast32_t */
 #define	PRIdFAST64	"ld"	/* int_fast64_t */
 #define	PRIdMAX		"jd"	/* intmax_t */
-#define	PRIdPTR		"ld"	/* intptr_t */
+#define	PRIdPTR		__PRIptr"d"	/* intptr_t */
 
 #define	PRIi8		"i"	/* int8_t */
 #define	PRIi16		"i"	/* int16_t */
@@ -66,7 +72,7 @@
 #define	PRIiFAST32	"i"	/* int_fast32_t */
 #define	PRIiFAST64	"li"	/* int_fast64_t */
 #define	PRIiMAX		"ji"	/* intmax_t */
-#define	PRIiPTR		"li"	/* intptr_t */
+#define	PRIiPTR		__PRIptr"i"	/* intptr_t */
 
 /* fprintf(3) macros for unsigned integers. */
 
@@ -83,7 +89,7 @@
 #define	PRIoFAST32	"o"	/* uint_fast32_t */
 #define	PRIoFAST64	"lo"	/* uint_fast64_t */
 #define	PRIoMAX		"jo"	/* uintmax_t */
-#define	PRIoPTR		"lo"	/* uintptr_t */
+#define	PRIoPTR		__PRIptr"o"	/* uintptr_t */
 
 #define	PRIu8		"u"	/* uint8_t */
 #define	PRIu16		"u"	/* uint16_t */
@@ -98,7 +104,7 @@
 #define	PRIuFAST32	"u"	/* uint_fast32_t */
 #define	PRIuFAST64	"lu"	/* uint_fast64_t */
 #define	PRIuMAX		"ju"	/* uintmax_t */
-#define	PRIuPTR		"lu"	/* uintptr_t */
+#define	PRIuPTR		__PRIptr"u"	/* uintptr_t */
 
 #define	PRIx8		"x"	/* uint8_t */
 #define	PRIx16		"x"	/* uint16_t */
@@ -113,7 +119,7 @@
 #define	PRIxFAST32	"x"	/* uint_fast32_t */
 #define	PRIxFAST64	"lx"	/* uint_fast64_t */
 #define	PRIxMAX		"jx"	/* uintmax_t */
-#define	PRIxPTR		"lx"	/* uintptr_t */
+#define	PRIxPTR		__PRIptr"x"	/* uintptr_t */
 
 #define	PRIX8		"X"	/* uint8_t */
 #define	PRIX16		"X"	/* uint16_t */
@@ -128,7 +134,7 @@
 #define	PRIXFAST32	"X"	/* uint_fast32_t */
 #define	PRIXFAST64	"lX"	/* uint_fast64_t */
 #define	PRIXMAX		"jX"	/* uintmax_t */
-#define	PRIXPTR		"lX"	/* uintptr_t */
+#define	PRIXPTR		__PRIptr"X"	/* uintptr_t */
 
 /* fscanf(3) macros for signed integers. */
 
@@ -145,7 +151,7 @@
 #define	SCNdFAST32	"d"	/* int_fast32_t */
 #define	SCNdFAST64	"ld"	/* int_fast64_t */
 #define	SCNdMAX		"jd"	/* intmax_t */
-#define	SCNdPTR		"ld"	/* intptr_t */
+#define	SCNdPTR		__PRIptr"d"	/* intptr_t */
 
 #define	SCNi8		"hhi"	/* int8_t */
 #define	SCNi16		"hi"	/* int16_t */
@@ -160,7 +166,7 @@
 #define	SCNiFAST32	"i"	/* int_fast32_t */
 #define	SCNiFAST64	"li"	/* int_fast64_t */
 #define	SCNiMAX		"ji"	/* intmax_t */
-#define	SCNiPTR		"li"	/* intptr_t */
+#define	SCNiPTR		__PRIptr"i"	/* intptr_t */
 
 /* fscanf(3) macros for unsigned integers. */
 
@@ -177,7 +183,7 @@
 #define	SCNoFAST32	"o"	/* uint_fast32_t */
 #define	SCNoFAST64	"lo"	/* uint_fast64_t */
 #define	SCNoMAX		"jo"	/* uintmax_t */
-#define	SCNoPTR		"lo"	/* uintptr_t */
+#define	SCNoPTR		__PRIptr"o"	/* uintptr_t */
 
 #define	SCNu8		"hhu"	/* uint8_t */
 #define	SCNu16		"hu"	/* uint16_t */
@@ -192,7 +198,7 @@
 #define	SCNuFAST32	"u"	/* uint_fast32_t */
 #define	SCNuFAST64	"lu"	/* uint_fast64_t */
 #define	SCNuMAX		"ju"	/* uintmax_t */
-#define	SCNuPTR		"lu"	/* uintptr_t */
+#define	SCNuPTR		__PRIptr"u"	/* uintptr_t */
 
 #define	SCNx8		"hhx"	/* uint8_t */
 #define	SCNx16		"hx"	/* uint16_t */
@@ -207,6 +213,6 @@
 #define	SCNxFAST32	"x"	/* uint_fast32_t */
 #define	SCNxFAST64	"lx"	/* uint_fast64_t */
 #define	SCNxMAX		"jx"	/* uintmax_t */
-#define	SCNxPTR		"lx"	/* uintptr_t */
+#define	SCNxPTR		__PRIptr"x"	/* uintptr_t */
 
 #endif /* !_MACHINE__INTTYPES_H_ */
