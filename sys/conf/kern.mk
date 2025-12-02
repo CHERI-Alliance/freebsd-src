7c693f7b128076525c159f10471c19e4a9aeec21
Brooks Davis <brooks@FreeBSD.org>
2025-12-02 12:35:45 +0000

    Add MACHINE_ARCH=aarch64c
    This adds make infrastructure support for the Arm Morello prototype
    which adds CHERI to Armv8.2.
    
    Co-authored-by:	Andrew Turner <andrew@fubar.geek.nz>
    Co-authored-by:	Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
    Co-authored-by:	Jessica Clarke <jrtc27@jrtc27.com>
    Effort:		CHERI upstreaming
    Sponsored by:	DARPA, AFRL, Innovate UK

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -152,6 +152,25 @@ LDFLAGS += -Wl,-zbti-report=error
 # TODO: support outline atomics
 CFLAGS += -mno-outline-atomics
 INLINE_LIMIT?=	8000
+
+.if ${MACHINE_CPU:Mcheri}
+CFLAGS+=	-march=morello
+# XXX: these items below should be ABI defaults
+CFLAGS+=	-Xclang -morello-vararg=new -Xclang -morello-bounded-memargs
+CFLAGS+=	-cheri-codeptr-relocs
+LDFLAGS+=	-cheri-codeptr-relocs
+LDFLAGS+=	-Wl,--local-caprelocs=elf
+
+.if ${MACHINE_ARCH:Maarch*c*}
+CFLAGS+=	-mabi=purecap
+.else
+CFLAGS+=	-mabi=aapcs
+.endif
+.endif
+
+.if ${MACHINE_ABI:Mpurecap}
+CFLAGS+=	-cheri-tgot-tls
+.endif
 .endif
 
 #
@@ -411,6 +430,7 @@ CCLDFLAGS+=	-fuse-ld=${LD:[1]:S/^ld.//1W}
 
 # Set target-specific linker emulation name.
 LD_EMULATION_aarch64=aarch64elf
+LD_EMULATION_aarch64c=aarch64elf
 LD_EMULATION_amd64=elf_x86_64_fbsd
 LD_EMULATION_arm=armelf_fbsd
 LD_EMULATION_armv7=armelf_fbsd
