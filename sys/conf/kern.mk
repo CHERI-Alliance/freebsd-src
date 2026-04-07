55e416f4ce292d58e877b6de7e2f407e8b9f7e16
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-04-07 11:48:58 +0100

    riscv64y: Temporarily disable Zifencei as it is not supported.

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -185,9 +185,11 @@ CFLAGS+=	-cheri-tgot-tls
 # code model as "medium" and "medany" respectively.
 #
 .if ${MACHINE_CPUARCH} == "riscv"
-RISCV_MARCH=	rv64imafdch_zifencei
+RISCV_MARCH=	rv64imafdch
 .if ${MACHINE_CPU:Mcheri}
 RISCV_MARCH:=	${RISCV_MARCH}xcheri
+.else
+RISCV_MARCH:=	${RISCV_MARCH}_zifencei
 .endif
 
 .if ${MACHINE_ARCH:Mriscv*c*}
