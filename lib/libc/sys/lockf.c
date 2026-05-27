80b712b11052a566d0e9fe3fa9d1f8b65bd4d6de
Brooks Davis <brooks@FreeBSD.org>
2026-05-27 17:46:47 +0100

    libc: use INTERPOS_SYS macro more in lockf
    lockf calls fcntl in two places and I only updated the one that
    followed the common pattern of returning directly.
    
    Fixes:		7dd9070e4425 ("libc: INTERPOS_SYS macro for interposed syscalls")

--- lib/libc/sys/lockf.c
+++ lib/libc/sys/lockf.c
@@ -62,9 +62,7 @@ lockf(int filedes, int function, off_t size)
 		break;
 	case F_TEST:
 		fl.l_type = F_WRLCK;
-		if (((int (*)(int, int, ...))
-		    *(__libc_interposing_slot(INTERPOS_fcntl)))
-		    (filedes, F_GETLK, &fl) == -1)
+		if (INTERPOS_SYS(fcntl, filedes, F_GETLK, (intptr_t)&fl) == -1)
 			return (-1);
 		if (fl.l_type == F_UNLCK || (fl.l_sysid == 0 &&
 		    fl.l_pid == getpid()))
