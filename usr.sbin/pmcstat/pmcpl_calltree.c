96c93862751ab8b0f7b875855b100881f61ee58d
Brooks Davis <brooks@FreeBSD.org>
2026-07-08 09:46:27 +0100

    pmcstat: silence warnings re pointers from addresses
    Use (void *)(intptr_t) casts (vs a bare (void *))when printing addresses
    to tell the compiler that we do mean to create provenance-free pointers
    for the purpose of printing them with %p.
    
    Effort:		CHERI upstreaming
    Sponsored by:	Innovate UK

--- usr.sbin/pmcstat/pmcpl_calltree.c
+++ usr.sbin/pmcstat/pmcpl_calltree.c
@@ -524,7 +524,7 @@ pmcpl_ct_node_printtop(struct pmcpl_ct_sample *rsamples, int pmcin, int maxy)
 				    pmcstat_string_unintern(ct->pct_sym->ps_name));
 			} else
 				ns_len = snprintf(ns, sizeof(ns), "%p",
-				    (void *)ct->pct_func);
+				    (void *)(intptr_t)ct->pct_func);
 
 			/* Format image. */
 			if (x == 1 ||
@@ -864,18 +864,20 @@ pmcpl_ct_node_printchild(struct pmcpl_ct_node *ct, ptraddr_t paddr,
 				        child->pct_sym->ps_name));
 			else
 				fprintf(args.pa_graphfile,
-				    "cfi=???\ncfn=%p\n", (void *)addr);
+				    "cfi=???\ncfn=%p\n",
+				    (void *)(intptr_t)addr);
 		}
 
 		/* Child function address, line and call count. */
 		fprintf(args.pa_graphfile, "calls=%u %p %u\n",
-		    ct->pct_arc[i].pcta_call, (void *)addr, line);
+		    ct->pct_arc[i].pcta_call, (void *)(intptr_t)addr, line);
 
 		/*
 		 * Call address, line, sample.
 		 * TODO: Associate call address to the right location.
 		 */
-		fprintf(args.pa_graphfile, "%p %u", (void *)paddr, pline);
+		fprintf(args.pa_graphfile, "%p %u", (void *)(intptr_t)paddr,
+		    pline);
 		for (j = 0; j<pmcstat_npmcs; j++)
 			fprintf(args.pa_graphfile, " %u",
 			    PMCPL_CT_SAMPLE(j, &ct->pct_arc[i].pcta_samples));
@@ -921,7 +923,7 @@ pmcpl_ct_node_printself(struct pmcpl_ct_node *ct)
 			    pmcstat_string_unintern(ct->pct_sym->ps_name));
 		else
 			fprintf(args.pa_graphfile, "fl=???\nfn=%p\n",
-			    (void *)(ct->pct_image->pi_vaddr + ct->pct_func));
+			    (void *)(intptr_t)(ct->pct_image->pi_vaddr + ct->pct_func));
 	}
 
 	/*
@@ -939,7 +941,7 @@ pmcpl_ct_node_printself(struct pmcpl_ct_node *ct)
 			    sourcefile, sizeof(sourcefile), &line,
 			    funcname, sizeof(funcname));
 			fprintf(args.pa_graphfile, "%p %u",
-			    (void *)addr, line);
+			    (void *)(intptr_t)addr, line);
 			for (j = 0; j<pmcstat_npmcs; j++)
 				fprintf(args.pa_graphfile, " %u",
 				    PMCPL_CT_SAMPLE(j,
@@ -948,7 +950,8 @@ pmcpl_ct_node_printself(struct pmcpl_ct_node *ct)
 		}
 	} else {
 		/* Global cost function cost. */
-		fprintf(args.pa_graphfile, "%p %u", (void *)faddr, fline);
+		fprintf(args.pa_graphfile, "%p %u", (void *)(intptr_t)faddr,
+		    fline);
 		for (i = 0; i<pmcstat_npmcs ; i++)
 			fprintf(args.pa_graphfile, " %u",
 			    PMCPL_CT_SAMPLE(i, &ct->pct_samples));
@@ -1058,7 +1061,7 @@ _pmcpl_ct_expand_inline(struct pmcpl_ct_node *ct)
 		if (args.pa_verbosity >= 2)
 			fprintf(args.pa_printfile,
 			    "WARNING: inlined function at %p %s in %s\n",
-			    (void *)addr, funcname, ffuncname);
+			    (void *)(intptr_t)addr, funcname, ffuncname);
 
 		snprintf(buffer, sizeof(buffer), "%s@%s",
 			funcname, ffuncname);
