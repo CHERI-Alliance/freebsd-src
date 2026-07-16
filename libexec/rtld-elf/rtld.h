2754a5e84da3d803faebe33b597077285ba1bd90
Brooks Davis <brooks@FreeBSD.org>
2026-07-16 18:12:13 +0100

    rtld: set defensive bounds on object arrays
    This prevents some types of rtld bugs from accessing outside the
    intended portion of the ELF object and ensures that invalid pointer
    constructions are out of bounds.
    
    Effort:		CHERI upstreaming
    Sponsored by:	DARPA, AFRL, Innovate UK

--- libexec/rtld-elf/rtld.h
+++ libexec/rtld-elf/rtld.h
@@ -482,6 +482,7 @@ extern bool ld_fast_sigblock;
 #ifdef __CHERI__
 bool create_pcc_caps(Obj_Entry *, const char *);
 const char *pcc_cap(const Obj_Entry *, Elf_Off);
+void narrow_object_bounds(Obj_Entry *obj);
 
 #define get_datasegment_cap(obj)				\
        (cheri_perms_clear((obj)->relocbase, CAP_RELOC_REMOVE_PERMS))
