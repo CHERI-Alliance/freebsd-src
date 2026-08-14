2efb23ed90927eefc34805adeafbc86769e58df4
Brooks Davis <brooks@one-eyed-alinet.net>
2026-08-14 14:00:35 +0100

    bsd.{prog,lib}.mk: use the TGOT ABI for CheriABI

--- share/mk/bsd.lib.mk
+++ share/mk/bsd.lib.mk
@@ -139,6 +139,11 @@ LDFLAGS+= -Wl,-zbti-report=error
 .endif
 .endif
 
+.if ${MACHINE_ABI:Mpurecap}
+# XXX: should be target default
+CFLAGS+=       -cheri-tgot-tls
+.endif
+
 # Initialize stack variables on function entry
 .if ${OPT_INIT_ALL} != "none"
 .if ${COMPILER_FEATURES:Minit-all}
