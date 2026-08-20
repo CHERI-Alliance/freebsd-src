CTSRD-CHERI/cheribsd

libc/aarch64: Fix build of string routines for purecap
.PATH finds the aarch64-specific versions of these files before the
"generic" versions that purecap needs to use.  Workaround by
explicitly #including the generic versions for purecap.

lib/libc/aarch64/string/strcat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../../string/strcat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -18,3 +21,4 @@ strcat(char * __restrict s, const char * __restrict append)
 
 	return(save);
 }
+#endif

lib/libc/aarch64/string/strlcat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../../string/strlcat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -23,3 +26,4 @@ strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	} else
 		return (dstsize + strlen(src));
 }
+#endif

lib/libc/aarch64/string/strncat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../../string/strncat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -27,3 +30,4 @@ strncat(char *dest, const char *src, size_t n)
 
 	return (dest);
 }
+#endif

lib/libc/aarch64/string/strpbrk.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../../string/strpbrk.c"
+#else
 /*-
  * Copyright (c) 2023 The FreeBSD Foundation
  *
@@ -41,3 +44,4 @@ strpbrk(const char *s, const char *charset)
 
 	return (s[loc] == '\0' ? NULL : (char *)&s[loc]);
 }
+#endif

lib/libc/aarch64/string/strsep.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI_PURE_CAPABILITY__
+#include "../../string/strsep.c"
+#else
 /*-
  * Copyright (c) 2023 The FreeBSD Foundation
  *
@@ -55,3 +58,4 @@ strsep(char **stringp, const char *delim)
 
 	return (s);
 }
+#endif

CHERI-Alliance/freebsd-src

lib/libc/aarch64/string/strcat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI__
+#include "../../string/strcat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -18,3 +21,4 @@ strcat(char * __restrict s, const char * __restrict append)
 
 	return(save);
 }
+#endif

lib/libc/aarch64/string/strlcat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI__
+#include "../../string/strlcat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -25,3 +28,4 @@ strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	} else
 		return (dstsize + strlen(src));
 }
+#endif

lib/libc/aarch64/string/strncat.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI__
+#include "../../string/strncat.c"
+#else
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
@@ -29,3 +32,4 @@ strncat(char *dest, const char *src, size_t n)
 
 	return (dest);
 }
+#endif

lib/libc/aarch64/string/strpbrk.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI__
+#include "../../string/strpbrk.c"
+#else
 /*-
  * Copyright (c) 2023 The FreeBSD Foundation
  *
@@ -41,3 +44,4 @@ strpbrk(const char *s, const char *charset)
 
 	return (s[loc] == '\0' ? NULL : (char *)&s[loc]);
 }
+#endif

lib/libc/aarch64/string/strsep.c
@@ -1,3 +1,6 @@
+#ifdef __CHERI__
+#include "../../string/strsep.c"
+#else
 /*-
  * Copyright (c) 2023 The FreeBSD Foundation
  *
@@ -55,3 +58,4 @@ strsep(char **stringp, const char *delim)
 
 	return (s);
 }
+#endif