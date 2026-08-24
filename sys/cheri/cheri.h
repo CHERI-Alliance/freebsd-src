8be52dbcaad6e113d50151410700590f37b066a7
Jessica Clarke <jrtc27@jrtc27.com>
2026-08-24 02:10:03 +0100

    fixup! cheri: create a userspace root capability

--- sys/cheri/cheri.h
+++ sys/cheri/cheri.h
@@ -44,30 +44,30 @@
  * Functions to construct userspace capabilities.
  */
 void *_cheri_capability_build_user_code(struct thread *td, uint32_t perms,
-    ptraddr_t basep, size_t length, off_t off, const char* func, int line);
+    ptraddr_t basep, size_t length, ptraddr_t addr, const char* func, int line);
 void *_cheri_capability_build_user_data(uint32_t perms, ptraddr_t basep,
-     size_t length, off_t off, const char* func, int line, bool exact);
+     size_t length, ptraddr_t addr, const char* func, int line, bool exact);
 void *_cheri_capability_build_user_rwx(uint32_t perms, ptraddr_t basep,
-    size_t length, off_t off, const char* func, int line, bool exact);
+    size_t length, ptraddr_t addr, const char* func, int line, bool exact);
 void *_cheri_capability_build_user_rwx_unchecked(uint32_t perms,
-    ptraddr_t basep, size_t length, off_t off, const char* func, int line,
+    ptraddr_t basep, size_t length, ptraddr_t addr, const char* func, int line,
     bool exact);
 
-#define cheri_capability_build_user_code(td, perms, basep, length, off)	\
-	_cheri_capability_build_user_code(td, perms, basep, length, off, \
+#define cheri_capability_build_user_code(td, perms, basep, length, addr)	\
+	_cheri_capability_build_user_code(td, perms, basep, length, addr, \
 	    __func__, __LINE__)
-#define cheri_capability_build_user_data(perms, basep, length, off)	\
-	_cheri_capability_build_user_data(perms, basep, length, off,	\
+#define cheri_capability_build_user_data(perms, basep, length, addr)	\
+	_cheri_capability_build_user_data(perms, basep, length, addr,	\
 	    __func__, __LINE__, true)
-#define cheri_capability_build_inexact_user_data(perms, basep, length, off) \
-	_cheri_capability_build_user_data(perms, basep, length, off,	\
+#define cheri_capability_build_inexact_user_data(perms, basep, length, addr) \
+	_cheri_capability_build_user_data(perms, basep, length, addr,	\
 	    __func__, __LINE__, false)
-#define cheri_capability_build_user_rwx(perms, basep, length, off)	\
-	_cheri_capability_build_user_rwx(perms, basep, length, off,	\
+#define cheri_capability_build_user_rwx(perms, basep, length, addr)	\
+	_cheri_capability_build_user_rwx(perms, basep, length, addr,	\
 	    __func__, __LINE__, true)
-#define cheri_capability_build_user_rwx_unchecked(perms, basep, length, off) \
+#define cheri_capability_build_user_rwx_unchecked(perms, basep, length, addr) \
 	_cheri_capability_build_user_rwx_unchecked(perms, basep, length, \
-	     off, __func__, __LINE__, true)
+	     addr, __func__, __LINE__, true)
 
 /*
  * Omnipotent capability for restoring swapped capabilities.
